f8cd4218d7b346da400fae80f754d45ce164fae2
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-01 18:11:19 +0100

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const API_KEY = "at_U9S38hGHy8KZnQMgwS7rbI4ZQw6p3";
 // change map location i.e add location icon to leaflet map
 const renderMarker = (lat: number, lng: number) => {
   const mIcon = L.icon({
-    iconUrl: "dist/images/icon-location.svg",
+    iconUrl: "https://shorturl.at/egoy5",
     iconSize: [60, 95],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
